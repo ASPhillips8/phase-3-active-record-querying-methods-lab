90138c4c2bfc0faffc0715f716fe8de83f4486ec
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-07-19 10:48:57 -0400

    Create class methods for order and conditions

--- app/models/show.rb
+++ app/models/show.rb
@@ -13,8 +13,18 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    Show.where(rating: self.lowest_rating).first
+    Show.find_by(rating: self.lowest_rating)
   end
 
+  def self.ratings_sum
+    Show.sum("rating")
+  end
 
+  def self.popular_shows
+    Show.where("rating > 5")
+  end
+
+  def self.shows_by_alphabetical_order
+    Show.order("name")
+  end
 end
