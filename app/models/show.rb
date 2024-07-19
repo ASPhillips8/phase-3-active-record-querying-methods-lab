8d3b3b12bd874521d40b18a6cdfba894ad02b031
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-07-19 10:25:34 -0400

    Create class methods for ratings

---
+++ app/models/show.rb
@@ -0,0 +1,20 @@
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    Show.find_by(rating: self.highest_rating)
+  end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    Show.where(rating: self.lowest_rating).first
+  end
+
+
+end
